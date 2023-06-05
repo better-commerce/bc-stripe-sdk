bd27639a01b73b1ec2e53b7b415f724e09687c81
Gagan <gagandeep.singh@bettercommerce.io>
2023-06-05 07:36:10 +0530

    Handle amount with decimals

--- src/stripe/Payment.ts
+++ src/stripe/Payment.ts
@@ -1,12 +1,13 @@
 import { StripeEnvironment } from "../base/config/StripeEnvironment";
 import { IPayment } from "../base/contracts/IPayment";
+import { sanitizeAmount } from "../utils/app-util";
 
 export class Payment implements IPayment {
 
     async initIntent(data: any): Promise<any> {
         const secretKey = StripeEnvironment.getSecretKey();
         const stripe = require("stripe")(secretKey);
-        const paymentIntentResult = await stripe.paymentIntents.create(data);
+        const paymentIntentResult = await stripe.paymentIntents.create({ ...data, ...{ amount: sanitizeAmount(data?.amount) } });
         return paymentIntentResult;
     }
 }
